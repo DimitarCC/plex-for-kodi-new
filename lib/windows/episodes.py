325e8c32ba0439607ed3a4e017cb0f2e83c96583
pannal <panni@fragstore.net>
2024-07-08 18:12:19 +0200

    Episodes/Movies: Fix play button not working after cancelling the resume dialog; fix play button not working after returning to view after direct playing from home

--- lib/windows/episodes.py
+++ lib/windows/episodes.py
@@ -874,8 +874,6 @@ class EpisodesWindow(kodigui.ControlledWindow, windowutils.UtilMixin, SeasonsMix
             util.messageDialog(T(32312, 'unavailable'), T(32332, 'This item is currently unavailable.'))
             return
 
-        self.playBtnClicked = True
-
         resume = False
         if episode.viewOffset.asInt():
             choice = dropdown.showDropdown(
@@ -895,6 +893,9 @@ class EpisodesWindow(kodigui.ControlledWindow, windowutils.UtilMixin, SeasonsMix
 
             if choice['key'] == 'resume':
                 resume = True
+
+        if not from_auto_play:
+            self.playBtnClicked = True
 
         pl = playlist.LocalPlaylist(self.show_.all(), self.show_.getServer())
         try:
